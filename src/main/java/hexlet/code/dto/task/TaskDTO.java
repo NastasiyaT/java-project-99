c57fdc24ac6cec3e95b65bfaaed4f102183ad912
Nastasiya Trusova <trusova.ab@yandex.ru>
2023-11-23 21:47:49 +0300

    fix json

--- src/main/java/hexlet/code/dto/task/TaskDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto.task;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,16 @@ import java.time.LocalDate;
 @Setter
 public class TaskDTO {
     private Long id;
+
+    @JsonProperty
     private Integer index;
+
     private LocalDate createdAt;
     private Long assigneeId;
     private String title;
+
+    @JsonProperty
     private String content;
+
     private String status;
 }

527c6dacee96fee8e6eb3f5a1539f66e0874ae14
Nastasiya Trusova <trusova.ab@yandex.ru>
2023-11-24 13:04:23 +0300

    update TaskRepository

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     Optional<Task> findFirstByNameOrderByIdDesc(String name);
+    Optional<Task> findByName(String name);
 }

dc198e7d1ac045a15a07c05e925a0cef76665aa2
Nastasiya Trusova <trusova.ab@yandex.ru>
2023-11-24 12:22:29 +0300

    fix Task

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
-    List<Task> findByName(String name);
+    Optional<Task> findByName(String name);
 }
